1b670864c53f87ba3d14006843d5fdd66f602060
Jagadeesh Gongidi <jagadeeshgoud001@gmail.com>
2023-08-16 23:34:57 +0530

    updated favicons

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 3500;
+
 // server used to send send emails
 
 
